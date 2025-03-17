ed7b9f24cd50960628e1481e54356268e7d60d9c
Vaibhav-Kolhe14 <vaibhavkolhe1404@gmail.com>
2025-03-17 20:02:05 +0530

    backend started

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(express.json({limit: '16kb'}))
+app.use(express.urlencoded({extended: true, limit: '16kb'}))
+
+app.get('/', (req, res) => {
+    res.status(200).json('success')
+})
+
+module.exports = { app }
