4b531cc8def99afe64ada71d1a529476c680ed6d
Vaibhav-Kolhe14 <vaibhavkolhe1404@gmail.com>
2025-03-22 10:38:44 +0530

    admin added

---
+++ admin/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+function App() {
+  const [token, setToken] = useState("");
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[20%] mx-auto my-8 text-gray-600 text-base ml-[max(5vw, 25px)]">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Login() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandlder = async(e) => {
+        try {
+            e.preventDefault();
+            const response = await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/user/admin`, {email, password})
+
+        } catch (error) {
+            
+        }
+    }
+
+  return (
+    <div className='min-h-screen flex items-center justify-center w-full'>
+      <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+        <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+        <form onSubmit={onSubmitHandlder}>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border text-gray-600 outline-none' type='email' placeholder='your@email.com' required />
+            </div>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border text-gray-600 outline-none' type='password' placeholder='Enter your password' required />
+            </div>
+            <button type='submit' className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black cursor-pointer'>Login</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
